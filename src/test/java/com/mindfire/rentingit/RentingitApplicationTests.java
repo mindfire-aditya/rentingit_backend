a3e57ab340a625e0fe63e0f3a6af0d9478db5f1b
Aditya Kumar Sahu <77772688+mindfire-aditya@users.noreply.github.com>
2021-03-16 15:18:09 +0530

    Initial commit

---
+++ src/test/java/com/mindfire/rentingit/RentingitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindfire.rentingit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentingitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
