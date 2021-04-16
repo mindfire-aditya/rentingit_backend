56f4c982852eb5a8a24d8b298e317d534375ec25
Aditya Kumar Sahu <77772688+mindfire-aditya@users.noreply.github.com>
2021-04-16 10:12:29 +0530

    Id field passed

--- src/main/java/com/mindfire/rentingit/repository/ProductRepository.java
+++ src/main/java/com/mindfire/rentingit/repository/ProductRepository.java
@@ -13,6 +13,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	Optional<Product> findByPinCode(int pincode);
 
-	List<Product> findByCategoryId();
+	List<Product> findByCategoryId(Long Id);
 
 }
