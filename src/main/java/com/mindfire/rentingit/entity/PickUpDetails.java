e6329b8795ed86c5668d8c6b3ea08053ecf54d60
Aditya Kumar Sahu <77772688+mindfire-aditya@users.noreply.github.com>
2021-04-16 11:15:47 +0530

    added Lombok and PickUpDetails

--- src/main/java/com/mindfire/rentingit/entity/PickUpDetails.java
+++ src/main/java/com/mindfire/rentingit/entity/PickUpDetails.java
@@ -1,5 +1,39 @@
 package com.mindfire.rentingit.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "PICKUP_DETAILS")
+@Data
 public class PickUpDetails {
+
+	private Long assetId;
+
+	private int pinCode;
+
+	private String houseNumber;
+	private String street;
+	private String lane;
+	private String landmark;
+	private String googleMapLocation;
+	private String cityTown;
+
+	public PickUpDetails(Long assetId, int pinCode, String houseNumber, String street, String lane, String landmark,
+			String googleMapLocation, String cityTown) {
+		super();
+		this.assetId = assetId;
+		this.pinCode = pinCode;
+		this.houseNumber = houseNumber;
+		this.street = street;
+		this.lane = lane;
+		this.landmark = landmark;
+		this.googleMapLocation = googleMapLocation;
+		this.cityTown = cityTown;
+	}
+	
+	
 
 }
