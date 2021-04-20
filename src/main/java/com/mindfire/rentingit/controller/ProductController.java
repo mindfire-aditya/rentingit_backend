617330e4f157f003cc4de6ce2a06acee5443c45e
Aditya Kumar Sahu <77772688+mindfire-aditya@users.noreply.github.com>
2021-04-20 22:01:53 +0530

    get products by owner id rest api

--- src/main/java/com/mindfire/rentingit/controller/ProductController.java
+++ src/main/java/com/mindfire/rentingit/controller/ProductController.java
@@ -59,6 +59,12 @@ public class ProductController {
 	public List<Product> getProductByCategoryId(@PathVariable(value = "id") int categoryId) {
 		return this.productRepository.findByCategoryId(categoryId);
 	}
+	
+	@GetMapping("/ownerId/{id}")
+	public List<Product> getProductsByOwnerId(@PathVariable(value = "id") int id) {
+		return productRepository.findByOwnerId(id);
+	}
+	
 
 	// get the product based upon Parent category name
 	@GetMapping("/category/parentcategory/{parentCategoryName}")
