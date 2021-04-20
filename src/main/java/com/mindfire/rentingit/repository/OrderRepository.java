2355174c64fc7605c5993f607efdbcfb653eacee
Aditya Kumar Sahu <77772688+mindfire-aditya@users.noreply.github.com>
2021-04-20 21:55:48 +0530

    added order fields in entity and update order respository

--- src/main/java/com/mindfire/rentingit/repository/OrderRepository.java
+++ src/main/java/com/mindfire/rentingit/repository/OrderRepository.java
@@ -1,5 +1,7 @@
 package com.mindfire.rentingit.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import com.mindfire.rentingit.entity.Order;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+	List<Order> findByOwnerId(int ownerId);
+	
+	List<Order> findByCustomerId(int customerId);
 }
